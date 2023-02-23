0e8df1b188e77bff1c5fd1477917486c7272685f
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2023-02-23 17:43:28 +0100

    Fixed installer

--- reset.sh
+++ reset.sh
@@ -1,6 +1,9 @@
 # Up docker compose
 sudo docker compose up -d
 
+# Wait for mariadb to start
+sleep 60
+
 # Drop databases and create databases
 sudo docker compose exec mariadb bash -c "mysql -u root -proot -e \"DROP DATABASE IF EXISTS payment; CREATE DATABASE payment CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;\""
 sudo docker compose exec mariadb bash -c "mysql -u root -proot -e \"DROP DATABASE IF EXISTS wordpress; CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;\""
